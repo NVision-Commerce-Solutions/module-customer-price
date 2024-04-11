43c153372880986144439095206c6c93aac3f3ca
alexsolonenko <oleksii.solonenko@leanswift.com>
2024-04-11 14:16:58 +0300

    CP-183 - remove old way of different connection usage, they were moved to Core module

--- Service/Request/GetCustomerPrices.php
+++ Service/Request/GetCustomerPrices.php
@@ -4,38 +4,56 @@ declare(strict_types=1);
 
 namespace Commerce365\CustomerPrice\Service\Request;
 
-use Commerce365\Core\Service\Request\Post;
+use Commerce365\Core\Model\AdvancedConfig;
+use Commerce365\Core\Service\Request\PostFactory;
 use Commerce365\CustomerPrice\Service\Customer\CurrencyResolver;
 
 class GetCustomerPrices implements GetCustomerPricesInterface
 {
-    private Post $post;
     private CurrencyResolver $currencyResolver;
+    private AdvancedConfig $advancedConfig;
+    private PostFactory $postFactory;
 
     /**
-     * @param Post $post
      * @param CurrencyResolver $currencyResolver
+     * @param AdvancedConfig $advancedConfig
      */
     public function __construct(
-        Post $post,
-        CurrencyResolver $currencyResolver
+        CurrencyResolver $currencyResolver,
+        AdvancedConfig $advancedConfig,
+        PostFactory $postFactory
     ) {
-        $this->post = $post;
         $this->currencyResolver = $currencyResolver;
+        $this->advancedConfig = $advancedConfig;
+        $this->postFactory = $postFactory;
     }
 
     public function execute($productIds, $customerId)
     {
-        $priceData = $this->post->execute('price', [
+        $post = $this->postFactory->create();
+
+        $priceData = $post->execute($this->getMethod(), [
             'json' => [
                 'CustomerId' => (int) $customerId,
                 'ProductIds' => array_values(array_map('intval', $productIds)),
                 'CurrencyCode' => $this->currencyResolver->resolve($customerId),
-                'UnitofMeasureCode' => ''
             ],
             'allow_redirects'=> ['strict' => true]
         ]);
 
         return !empty($priceData['priceLists']) ? $priceData['priceLists'] : [];
+    }
+
+    private function getMethod(): string
+    {
+        if ($this->advancedConfig->isBCOAuth()) {
+            return 'GetPrices_GetPricesById';
+        }
+
+        if ($this->advancedConfig->isBCBasic()) {
+            return 'GetPricesMock_GetPricesByIdMock';
+        }
+
+        return 'price';
     }
 }
